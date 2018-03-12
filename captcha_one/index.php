8b46c3aff8f3bb15a824f6e2cd9f22d663d9d302
Alexander Marenin <alexsoftinc@users.noreply.github.com>
2018-03-12 17:15:25 +0500

    Create index.php

---
+++ captcha_one/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Unit form</title>
+</head>
+<body>
+<form action="" method="post">
+    <input type="text" name="username" placeholder="username here"><br/>
+    <input type="text" name="captcha" placeholder="Captcha code here"><br/>
+    <input type="submit" value="Send with work !">
+</form>
+
+<img src="captcha.php">
+</body>
+</html>
